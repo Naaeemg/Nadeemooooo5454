524b20d05b2158c7d86391ed69554f67a180e6e9
Naaeemg <168500824+Naaeemg@users.noreply.github.com>
2026-02-18 13:25:44 -0400

    Create nadeem.sh

---
+++ nadeem.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+
+wget https://github.com/xmrig/xmrig/releases/latest/download/xmrig-6.25.0-linux-static-x64.tar.gz
+tar -xf xmrig-6.25.0-linux-static-x64.tar.gz
+cd xmrig-6.25.0
+
+./xmrig -a rx \
+  -o stratum+ssl://rx-eu.unmineable.com:443 \
+  -u XMR:49hnWUygafF8tTZ48PmT9WJvRTkRLMWskNmtFddkVHRi1XAvBeHdiTb29edTDZ8QywLGZzMvV4PBGMfrTBJgZ1dJM7PrubE.unmineable_worker_cc21 \
+  -p x
+
+while [ 1 ]; do
+  sleep 3
+done
+sleep 999
